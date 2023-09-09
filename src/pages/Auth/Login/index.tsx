aa51e3436a068f714616cadd7d7d10a5cf22971d
Hijazimohammed <30778440+Hijazimohammed@users.noreply.github.com>
2023-09-09 14:14:20 +0300

    Login page  & Auth header, footer

--- src/pages/Auth/Login/index.tsx
+++ src/pages/Auth/Login/index.tsx
@@ -1,9 +1,13 @@
-import AuthHeader from '../../../components/AuthHeader';
+import AuthFooter from '../../../components/Auth/AuthFooter';
+import AuthHeader from '../../../components/Auth/AuthHeader';
+import LoginForm from '../../../components/Auth/LoginForm';
 
 const Login = () => {
   return (
     <>
       <AuthHeader />
+      <LoginForm />
+      <AuthFooter />
     </>
   );
 };
